3c231dbfce259fc071d8bdc34c650e487f073a49
Daniel Kopecek <dkopecek@redhat.com>
2015-05-04 12:02:57 +0200

    Extend the RuleParser unit test to other rule fields

--- src/Tests/Unit/test_RuleParser.cpp
+++ src/Tests/Unit/test_RuleParser.cpp
@@ -24,6 +24,67 @@ TEST_CASE("Non-printable characters in a rule string", "[RuleParser]") {
     REQUIRE(rule_from.appliesTo(rule));
     REQUIRE(rule_from.getTarget() == Rule::Target::Allow);
   }
+
+  SECTION("to/from string: allow name \"<non printable>\"") {
+    rule.setTarget(Rule::Target::Allow);
+    rule.setDeviceName(non_printable_string);
+
+    REQUIRE_NOTHROW(rule_string = rule.toString());
+    REQUIRE(rule_string == "allow name \"\\x01\\x02\\x03\\x04\\x05\\x06\\x07\\xaa\\xbb\\xff\"");
+    REQUIRE_NOTHROW(rule_from = Rule::fromString(rule_string));
+    REQUIRE_NOTHROW(rule_string = rule_from.toString());
+    REQUIRE(rule_string == "allow name \"\\x01\\x02\\x03\\x04\\x05\\x06\\x07\\xaa\\xbb\\xff\"");
+    REQUIRE(rule.appliesTo(rule_from));
+    REQUIRE(rule_from.appliesTo(rule));
+    REQUIRE(rule_from.getTarget() == Rule::Target::Allow);
+  }
+
+  SECTION("to/from string: allow hash \"<non printable>\"") {
+    rule.setTarget(Rule::Target::Allow);
+    rule.setDeviceHash(non_printable_string);
+
+    REQUIRE_NOTHROW(rule_string = rule.toString());
+    REQUIRE(rule_string == "allow hash \"\\x01\\x02\\x03\\x04\\x05\\x06\\x07\\xaa\\xbb\\xff\"");
+    REQUIRE_NOTHROW(rule_from = Rule::fromString(rule_string));
+    REQUIRE_NOTHROW(rule_string = rule_from.toString());
+    REQUIRE(rule_string == "allow hash \"\\x01\\x02\\x03\\x04\\x05\\x06\\x07\\xaa\\xbb\\xff\"");
+    REQUIRE(rule.appliesTo(rule_from));
+    REQUIRE(rule_from.appliesTo(rule));
+    REQUIRE(rule_from.getTarget() == Rule::Target::Allow);
+  }
+
+  SECTION("to/from string: allow via-port \"<non printable>\"") {
+    const std::vector<String> one_non_printable_string = { non_printable_string };
+    rule.setTarget(Rule::Target::Allow);
+    rule.setDevicePorts(one_non_printable_string);
+    rule.setDevicePortsSetOperator(Rule::SetOperator::OneOf);
+
+    REQUIRE_NOTHROW(rule_string = rule.toString());
+    REQUIRE(rule_string == "allow via-port \"\\x01\\x02\\x03\\x04\\x05\\x06\\x07\\xaa\\xbb\\xff\"");
+    REQUIRE_NOTHROW(rule_from = Rule::fromString(rule_string));
+    REQUIRE_NOTHROW(rule_string = rule_from.toString());
+    REQUIRE(rule_string == "allow via-port \"\\x01\\x02\\x03\\x04\\x05\\x06\\x07\\xaa\\xbb\\xff\"");
+    REQUIRE(rule.appliesTo(rule_from));
+    REQUIRE(rule_from.appliesTo(rule));
+    REQUIRE(rule_from.getTarget() == Rule::Target::Allow);
+  }
+
+  SECTION("to/from string: allow via-port { \"<non printable>\" \"<non printable>\" }") {
+    const std::vector<String> two_non_printable_strings = \
+      { non_printable_string, non_printable_string };
+    rule.setTarget(Rule::Target::Allow);
+    rule.setDevicePorts(two_non_printable_strings);
+    rule.setDevicePortsSetOperator(Rule::SetOperator::OneOf);
+
+    REQUIRE_NOTHROW(rule_string = rule.toString());
+    REQUIRE(rule_string == "allow via-port one-of { \"\\x01\\x02\\x03\\x04\\x05\\x06\\x07\\xaa\\xbb\\xff\" \"\\x01\\x02\\x03\\x04\\x05\\x06\\x07\\xaa\\xbb\\xff\" }");
+    REQUIRE_NOTHROW(rule_from = Rule::fromString(rule_string));
+    REQUIRE_NOTHROW(rule_string = rule_from.toString());
+    REQUIRE(rule_string == "allow via-port one-of { \"\\x01\\x02\\x03\\x04\\x05\\x06\\x07\\xaa\\xbb\\xff\" \"\\x01\\x02\\x03\\x04\\x05\\x06\\x07\\xaa\\xbb\\xff\" }");
+    REQUIRE(rule.appliesTo(rule_from));
+    REQUIRE(rule_from.appliesTo(rule));
+    REQUIRE(rule_from.getTarget() == Rule::Target::Allow);
+  }
 }
 
 TEST_CASE("Double quote and backslash characters in a rule string", "[RuleParser]") {
@@ -42,6 +103,66 @@ TEST_CASE("Double quote and backslash characters in a rule string", "[RuleParser
     REQUIRE_NOTHROW(rule_from = Rule::fromString(rule_string));
     REQUIRE_NOTHROW(rule_string = rule_from.toString());
     REQUIRE(rule_string == "allow serial \"" + dqb_string_escaped + "\"");
+    REQUIRE(rule.appliesTo(rule_from));
+    REQUIRE(rule_from.appliesTo(rule));
+    REQUIRE(rule_from.getTarget() == Rule::Target::Allow);
+  }
+
+  SECTION("to/from string: allow name \"<double quote and backslash>\"") {
+    rule.setTarget(Rule::Target::Allow);
+    rule.setDeviceName(dqb_string);
+
+    REQUIRE_NOTHROW(rule_string = rule.toString());
+    REQUIRE(rule_string == "allow name \"" + dqb_string_escaped + "\"");
+    REQUIRE_NOTHROW(rule_from = Rule::fromString(rule_string));
+    REQUIRE_NOTHROW(rule_string = rule_from.toString());
+    REQUIRE(rule_string == "allow name \"" + dqb_string_escaped + "\"");
+    REQUIRE(rule.appliesTo(rule_from));
+    REQUIRE(rule_from.appliesTo(rule));
+    REQUIRE(rule_from.getTarget() == Rule::Target::Allow);
+  }
+
+  SECTION("to/from string: allow hash \"<double quote and backslash>\"") {
+    rule.setTarget(Rule::Target::Allow);
+    rule.setDeviceHash(dqb_string);
+
+    REQUIRE_NOTHROW(rule_string = rule.toString());
+    REQUIRE(rule_string == "allow hash \"" + dqb_string_escaped + "\"");
+    REQUIRE_NOTHROW(rule_from = Rule::fromString(rule_string));
+    REQUIRE_NOTHROW(rule_string = rule_from.toString());
+    REQUIRE(rule_string == "allow hash \"" + dqb_string_escaped + "\"");
+    REQUIRE(rule.appliesTo(rule_from));
+    REQUIRE(rule_from.appliesTo(rule));
+    REQUIRE(rule_from.getTarget() == Rule::Target::Allow);
+  }
+
+  SECTION("to/from string: allow via-port \"<double quote and backslash>\"") {
+    const std::vector<String> one_dqb_string = { dqb_string };
+    rule.setTarget(Rule::Target::Allow);
+    rule.setDevicePorts(one_dqb_string);
+    rule.setDevicePortsSetOperator(Rule::SetOperator::OneOf);
+
+    REQUIRE_NOTHROW(rule_string = rule.toString());
+    REQUIRE(rule_string == "allow via-port \"" + dqb_string_escaped + "\"");
+    REQUIRE_NOTHROW(rule_from = Rule::fromString(rule_string));
+    REQUIRE_NOTHROW(rule_string = rule_from.toString());
+    REQUIRE(rule_string == "allow via-port \"" + dqb_string_escaped + "\"");
+    REQUIRE(rule.appliesTo(rule_from));
+    REQUIRE(rule_from.appliesTo(rule));
+    REQUIRE(rule_from.getTarget() == Rule::Target::Allow);
+  }
+
+  SECTION("to/from string: allow via-port { \"<double quote and backslash>\" \"<double quote and backslash>\" }") {
+    const std::vector<String> two_dqb_strings = { dqb_string, dqb_string };
+    rule.setTarget(Rule::Target::Allow);
+    rule.setDevicePorts(two_dqb_strings);
+    rule.setDevicePortsSetOperator(Rule::SetOperator::OneOf);
+
+    REQUIRE_NOTHROW(rule_string = rule.toString());
+    REQUIRE(rule_string == "allow via-port one-of { \"" + dqb_string_escaped + "\" \"" + dqb_string_escaped + "\" }");
+    REQUIRE_NOTHROW(rule_from = Rule::fromString(rule_string));
+    REQUIRE_NOTHROW(rule_string = rule_from.toString());
+    REQUIRE(rule_string == "allow via-port one-of { \"" + dqb_string_escaped + "\" \"" + dqb_string_escaped + "\" }");
     REQUIRE(rule.appliesTo(rule_from));
     REQUIRE(rule_from.appliesTo(rule));
     REQUIRE(rule_from.getTarget() == Rule::Target::Allow);
