dc699c5b4a0a7ec69f74963dc932b3f642f37a39
Daniel Kopecek <dkopecek@redhat.com>
2015-05-04 10:28:56 +0200

    Test double quote and backslash escaping

--- src/Tests/Unit/test_RuleParser.cpp
+++ src/Tests/Unit/test_RuleParser.cpp
@@ -25,3 +25,25 @@ TEST_CASE("Non-printable characters in a rule string", "[RuleParser]") {
     REQUIRE(rule_from.getTarget() == Rule::Target::Allow);
   }
 }
+
+TEST_CASE("Double quote and backslash characters in a rule string", "[RuleParser]") {
+  Rule rule, rule_from;
+  std::string rule_string;
+
+  const std::string dqb_string("\" \\ \"\" \\\\ \\\" \"\\");
+  const std::string dqb_string_escaped("\\\" \\\\ \\\"\\\" \\\\\\\\ \\\\\\\" \\\"\\\\");
+
+  SECTION("to/from string: allow serial \"<double quote and backslash>\"") {
+    rule.setTarget(Rule::Target::Allow);
+    rule.setSerialNumber(dqb_string);
+
+    REQUIRE_NOTHROW(rule_string = rule.toString());
+    REQUIRE(rule_string == "allow serial \"" + dqb_string_escaped + "\"");
+    REQUIRE_NOTHROW(rule_from = Rule::fromString(rule_string));
+    REQUIRE_NOTHROW(rule_string = rule_from.toString());
+    REQUIRE(rule_string == "allow serial \"" + dqb_string_escaped + "\"");
+    REQUIRE(rule.appliesTo(rule_from));
+    REQUIRE(rule_from.appliesTo(rule));
+    REQUIRE(rule_from.getTarget() == Rule::Target::Allow);
+  }
+}
