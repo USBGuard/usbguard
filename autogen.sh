965bdc41dc4460a3e6d19447c2dcdd35425e80d5
Daniel Kopecek <dkopecek@redhat.com>
2015-08-27 16:59:35 +0200

    Create m4 subdir for every autoconf based project subdir

--- autogen.sh
+++ autogen.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -x -e
 git submodule update --init
-mkdir -p m4
 for reconf_dir in "./" "./src/ThirdParty/libqb" "./src/ThirdParty/libsodium"; do
-    autoreconf -i -s --no-recursive "$reconf_dir"
+  mkdir -p "$reconf_dir/m4"
+  autoreconf -i -s --no-recursive "$reconf_dir"
 done
