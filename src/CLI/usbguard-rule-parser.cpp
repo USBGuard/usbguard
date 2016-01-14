bedd2623ef7de33fd463b0a1f81f53c5c6f1e779
Daniel Kopecek <dnk1618@gmail.com>
2016-01-15 00:14:44 +0100

    Added a rule parser tool

---
+++ src/CLI/usbguard-rule-parser.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#ifndef _GNU_SOURCE
+# define _GNU_SOURCE
+#endif
+#include <cstring>
+#include "Rule.hpp"
+#include "RuleParser.hpp"
+
+int main(int argc, char **argv)
+{
+  if (argc != 2) {
+    std::cerr << "Usage: " << ::basename(argv[0]) << " <rulespec>" << std::endl;
+  }
+  else {
+    try {
+      const std::string rule_spec(argv[1]);
+      const usbguard::Rule rule = usbguard::parseRuleSpecification(rule_spec);
+      return EXIT_SUCCESS;
+    }
+    catch(const usbguard::RuleParserError& ex) {
+      std::cerr << "ERROR: " << ex.what() << std::endl;
+      std::cerr << "HINT: " << ex.hint() << std::endl;
+      if (ex.hasFileInfo()) {
+        std::cerr << "FILE: " << ex.fileInfo() << std::endl;
+      }
+    }
+    catch(const std::exception& ex) {
+      std::cerr << "EXCEPTION: " << ex.what() << std::endl;
+    }
+    catch(...) {
+      std::cerr << "EXCEPTION: Unknown" << std::endl;
+    }
+  }
+  return EXIT_FAILURE;
+}
