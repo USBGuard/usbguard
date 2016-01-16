ad84e14336e146ce6f731566c1b6450612997483
Daniel Kopecek <dnk1618@gmail.com>
2016-01-16 19:05:00 +0100

    Refactored autoconf/automake stuff
     * removed the possibility to build bundled sodium and qb libraries
     * removed libqb and libsodium submodules
     * removed src/build-config.h.in from the repository

--- autogen.sh
+++ autogen.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 set -x -e
 git submodule update --init
-for reconf_dir in "./" "./src/ThirdParty/libqb" "./src/ThirdParty/libsodium"; do
-  mkdir -p "$reconf_dir/m4"
-  autoreconf -i -s --no-recursive "$reconf_dir"
-done
+mkdir -p ./m4
+autoreconf -i -s --no-recursive ./
+
