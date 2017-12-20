03cb3d3f199aacee57f6add946fb2d19d4a9fc57
Radovan Sroka <rsroka@redhat.com>
2017-12-20 10:40:06 +0100

    KeyValueParser enhancements (#216)
    - now it is possible to call parseStream with ifstream parameter
    (opened file)
    - added possibility to check keys as case insensitive

--- src/Library/KeyValueParser.cpp
+++ src/Library/KeyValueParser.cpp
@@ -15,8 +15,10 @@
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 //
 // Authors: Daniel Kopecek <dkopecek@redhat.com>
-// Authors: Marek Tamaskovic <mtamasko@redhat.com>
+//          Marek Tamaskovic <mtamasko@redhat.com>
+//          Radovan Sroka <rsroka@redhat.com>
 //
+
 #ifdef HAVE_BUILD_CONFIG_H
   #include <build-config.h>
 #endif
@@ -27,20 +29,28 @@
 #include "usbguard/Exception.hpp"
 #include "usbguard/Logger.hpp"
 
+#include <locale>
+
 namespace usbguard
 {
-  KeyValueParser::KeyValueParser(const std::vector<std::string>& v, const std::string& sep):
-    keys(v), separator(sep) {}
+  /**
+  * NOTE:
+  * vector v should have strings UPPERCASE when case insensitive mode was enabled
+  */
+  KeyValueParser::KeyValueParser(const std::vector<std::string>& v, const std::string& sep, bool case_sensitive):
+    _keys(v), _separator(sep), _case_sensitive(case_sensitive)
+  {
+  }
 
-  KeyValueParser::KeyValueParser(const std::vector<std::string>& v):
-    KeyValueParser(v, "=") {}
+  KeyValueParser::KeyValueParser(const std::vector<std::string>& v, bool case_sensitive):
+    KeyValueParser(v, "=", case_sensitive) {}
 
   void KeyValueParser::viewConfig()
   {
-    std::cout << "separator:\t" << this->separator << std::endl;
+    std::cout << "separator:\t" << this->_separator << std::endl;
     std::cout << "keys:\t";
 
-    for (auto item : this->keys) {
+    for (auto item : this->_keys) {
       std::cout << item << ", ";
     }
 
@@ -51,7 +61,7 @@ namespace usbguard
   {
     std::string::size_type sep_pos;
     std::string key_c, key, val;
-    sep_pos = str.find(this->separator);
+    sep_pos = str.find(this->_separator);
 
     if (sep_pos == std::string::npos) {
       throw Exception("KeyValueParser", "Parser", "Separator not found: syntax error");
@@ -61,6 +71,12 @@ namespace usbguard
       val = str.substr(sep_pos + 1);
       key = trim(key);
       val = trim(val);
+
+      if (!this->_case_sensitive) {
+        for (size_t i = 0 ; i < key.length() ; i++) {
+          key[i] = std::toupper(key[i], this->_loc);
+        }
+      }
 
       if (this->checkKeyValidity(key)) {
         USBGUARD_LOG(Error) << "Error: parsed key is not in key set: '" << key << "'";
@@ -72,7 +88,7 @@ namespace usbguard
     }
   }
 
-  bool KeyValueParser::parseStream(std::fstream& stream)
+  void KeyValueParser::parseStream(std::istream& stream)
   {
     std::map<std::string, std::string> m;
     std::map<std::string, std::string>::iterator it;
@@ -94,19 +110,12 @@ namespace usbguard
       }
     }
 
-    this->output_map = std::move(m);
-
-    if (this->checkMapValidity()) {
-      return false;
-    }
-    else {
-      return true;
-    }
+    this->_output_map = std::move(m);
   }
 
   bool KeyValueParser::checkKeyValidity(const std::string& key)
   {
-    for (auto a: this->keys) {
+    for (auto a: this->_keys) {
       if (!key.compare(a)) {
         return false;
       }
@@ -117,13 +126,7 @@ namespace usbguard
 
   std::map<std::string, std::string> KeyValueParser::getMap()
   {
-    return this->output_map;
-  }
-
-  bool KeyValueParser::checkMapValidity()
-  {
-    /* Some difficult checks in map*/
-    return true;
+    return this->_output_map;
   }
 
 } /* namespace usbguard */
